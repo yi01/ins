551d4ddc5c2f15607f473cffbdea9b7634e83767
yuhei ikuzawa <ikuzawayuhei@ikuzawayuutaira-no-MacBook-Air.local>
2017-06-05 12:57:07 +0900

    2

---
+++ app/mailers/notice_mailer.rb
@@ -0,0 +1,14 @@
+class NoticeMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.notice_mailer.sendmail_picture.subject
+  #
+  def sendmail_picture(picture)
+    @picture = picture
+
+    mail to: "yi0123456789@icloud.com"
+      subject: "ok"
+  end
+end
